a87aeaccd4cab5f0f5aa6f09bf39e50b810aaeed
Bilel-Gharbi <hb.bilel@gmail.com>
2020-05-22 01:57:28 +0100

    deploy v1.0

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const path = require("path");
 const loader = require("./loaders");
-const { PORT } = require("./config");
 
 const app = express();
 loader(app);
 
-app.listen(PORT, err =>
-  err ? console.log(err) : console.log(`server is running on port ${PORT}`)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+let port = process.env.PORT || 5000;
+app.listen(port, (err) =>
+  err ? console.log(err) : console.log(`server is running on port ${port}`)
 );
